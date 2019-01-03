bf944b0ab8ecf48128328ca1c611a4946e495c20
Alexander Bueckig <AlexBueckig@gmail.com>
2019-01-03 10:15:11 +0100

    Routen implementiert

--- packages/client/src/containers/App/index.tsx
+++ packages/client/src/containers/App/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import Adventcalendar from '../Adventcalendar';
+import CalendarEdit from '../CalendarEdit';
+import CalendarOverview from '../CalendarOverview';
+import Home from '../Home';
+
 import './App.scss';
 
 class App extends React.Component {
@@ -10,7 +16,14 @@ class App extends React.Component {
           <h1>Adventskalender</h1>
         </header>
         <main>
-          <Adventcalendar />
+          <div className="content">
+            <Switch>
+              <Route exact={true} path="/" component={Home} />
+              <Route exact={true} path="/calendar/edit/:id" component={CalendarEdit} />
+              <Route exact={true} path="/calendar/:id" component={Adventcalendar} />
+              <Route path="/calendars" component={CalendarOverview} />
+            </Switch>
+          </div>
         </main>
         <footer>Footer</footer>
       </div>
